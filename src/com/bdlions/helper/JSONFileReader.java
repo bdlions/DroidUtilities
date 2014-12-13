a88c11bf30a4b91e2d901a469c49d97e3c6574f0
Alamgir Kabir <alamgirkabir7@gmail.com>
2014-12-13 15:45:28 +0600

    JsonFilereader to read a file and return as string

---
+++ src/com/bdlions/helper/JSONFileReader.java
@@ -0,0 +1,43 @@
+package com.bdlions.helper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+
+import android.content.Context;
+
+
+public class JSONFileReader {
+
+	public static String getContent(Context context, int resource){
+		InputStream is = context.getResources().openRawResource(resource);
+		Writer writer = new StringWriter();
+		char[] buffer = new char[1024];
+		try {
+		    Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+		    int n;
+		    while ((n = reader.read(buffer)) != -1) {
+		        writer.write(buffer, 0, n);
+		    }
+		    System.out.println(buffer);
+		    is.close();
+		}
+		catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+		    
+		}
+
+		return  writer.toString();
+	}
+}
